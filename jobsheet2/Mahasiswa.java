e4415ffaa935f552af8aabfade7eeeb121b9e59b
dityans <dityan25@gmail.com>
2026-02-18 10:52:23 +0700

    Percobaan 1, Modifikasi 1

--- jobsheet2/Mahasiswa.java
+++ jobsheet2/Mahasiswa.java
@@ -16,7 +16,11 @@ public class Mahasiswa {
         kelas = kelasBaru;
     }
     void updateIpk (double ipkBaru) {
-        ipk = ipkBaru;
+        if (ipk >= 0 && ipk <= 4){
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
     }
     String nilaiKinerja () {
         if (ipk >= 3.5) {
