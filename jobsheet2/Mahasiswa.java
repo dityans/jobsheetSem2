7612a7333b0568adaa03cdd4850e77e64b7d2474
dityans <dityan25@gmail.com>
2026-02-18 13:37:07 +0700

    debugging

--- jobsheet2/Mahasiswa.java
+++ jobsheet2/Mahasiswa.java
@@ -16,7 +16,7 @@ public class Mahasiswa {
         kelas = kelasBaru;
     }
     void updateIpk (double ipkBaru) {
-        if (ipk >= 0 && ipk <= 4){
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0){
             ipk = ipkBaru;
         } else {
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
