7bdfd675bc22c81c37e4f2fe0360cce155eff2a3
dityans <dityan25@gmail.com>
2026-02-25 13:33:37 +0700

    Percobaan 2, Modifikasi 1

--- jobsheet3/MahasiswaDemo07.java
+++ jobsheet3/MahasiswaDemo07.java
@@ -25,11 +25,7 @@ public class MahasiswaDemo07 {
 
         for (int i = 0; i < arrayOfMahasiswa.length; i++){
             System.out.println("Data mahasiswa ke-" + (i+1));
-            System.out.println("NIM     : " + arrayOfMahasiswa[i].nim);
-            System.out.println("Nama    : " + arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas   : " + arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK     : " + arrayOfMahasiswa[i].ipk);
-            System.out.println("------------------------------");
+            arrayOfMahasiswa[i].cetakInfo();
         }
         sc.close();
     }
