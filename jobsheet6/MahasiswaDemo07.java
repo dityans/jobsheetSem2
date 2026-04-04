06baa282cceb046a715ddab08f26f04d48b1c885
dityans <dityan25@gmail.com>
2026-04-04 20:31:15 +0700

    3.4 mahasiswa insertion sort

--- jobsheet6/MahasiswaDemo07.java
+++ jobsheet6/MahasiswaDemo07.java
@@ -33,6 +33,10 @@ public class MahasiswaDemo07 {
         list.selectionSort();
         list.tampil();
 
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
+        list.tampil();
+        
         sc.close();
     }
 }
