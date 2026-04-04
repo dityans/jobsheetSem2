8a49176af3da1c8c0ce04153c09a26ea6ff38d7a
dityans <dityan25@gmail.com>
2026-04-04 10:29:06 +0700

    praktikum 1 insertion sort

--- jobsheet6/sortingMain07.java
+++ jobsheet6/sortingMain07.java
@@ -4,6 +4,7 @@ public class sortingMain07 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
         int b[] ={30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
 
         sorting07 dataUrut1 = new sorting07(a, a.length);
         System.out.println("Data awal 1");
@@ -19,5 +20,13 @@ public class sortingMain07 {
         dataUrut2.selectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataUrut2.tampil();
+        System.out.println();
+
+        sorting07 dataUrut3 = new sorting07(c, c.length);
+        System.out.println("Data awal 3");
+        dataUrut3.tampil();
+        dataUrut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataUrut3.tampil();
     }
 }
