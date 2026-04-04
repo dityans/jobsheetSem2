62905fde001df1c1874d7a7be6cba037bd7443c2
dityans <dityan25@gmail.com>
2026-04-04 09:00:58 +0700

    Praktikum 1 selection sort

--- jobsheet6/sortingMain07.java
+++ jobsheet6/sortingMain07.java
@@ -3,6 +3,7 @@ package jobsheet6;
 public class sortingMain07 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
+        int b[] ={30, 20, 2, 8, 14};
 
         sorting07 dataUrut1 = new sorting07(a, a.length);
         System.out.println("Data awal 1");
@@ -10,5 +11,13 @@ public class sortingMain07 {
         dataUrut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataUrut1.tampil();
+        System.out.println();
+
+        sorting07 dataUrut2 = new sorting07(b, b.length);
+        System.out.println("Data awal 2");
+        dataUrut2.tampil();
+        dataUrut2.selectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataUrut2.tampil();
     }
 }
