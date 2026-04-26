18a12493330a9b2512618ff8653df4351c3fffe7
dityans <dityan25@gmail.com>
2026-04-26 10:47:58 +0700

    Jobsheet 9 Percobaan 2

--- Jobsheet9/MahasiswaDemo07.java
+++ Jobsheet9/MahasiswaDemo07.java
@@ -37,6 +37,8 @@ public class MahasiswaDemo07 {
                         int nilai = sc.nextInt();
                         dinilai.tugasDinilai(nilai);
                         System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                        String biner = stack.konversiDesimalBiner(nilai);
+                        System.out.println("Nilai biner tugas: " + biner);
                     }
                     break;
                 case 3:
